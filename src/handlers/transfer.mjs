c3b2b73761b3cbbaadc3f0bfe3b7a72ef05ed14b
Mike <korostelevm@gmail.com>
2023-12-12 16:44:46 -0500

    tests

---
+++ src/handlers/transfer.mjs
@@ -0,0 +1,72 @@
+// Create clients and set shared const values outside of the handler.
+
+// Create a DocumentClient that represents the query to add an item
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand, QueryCommand, GetCommand } from '@aws-sdk/lib-dynamodb';
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+export const transferHandler = async (event, context) => {
+
+    console.log(JSON.stringify({event, context},null,2))
+
+    let user = event.requestContext.authorizer.claims['custom:username']
+    let requested_account_name = event.pathParameters.account_name
+    
+    let q = {
+        TableName: process.env.DB,
+        KeyConditionExpression: "pk=:pk",
+        FilterExpression: "account_name = :account_name",
+        ExpressionAttributeValues: {
+            ":pk": `${user}`,
+            ":account_name": `${requested_account_name}`
+        },
+        
+    }
+    let accounts_query = await ddbDocClient.send(new QueryCommand(q))
+    let accounts = accounts_query.Items
+
+    if(!accounts.length){
+        return {
+            statusCode: 404,
+            body: JSON.stringify({
+                message: `Account ${requested_account_name} not found for user ${user}`
+            })
+        }
+    }
+
+    let account = accounts_query.Items[0]
+
+    let account_details = await ddbDocClient.send(new GetCommand({
+        TableName: process.env.DB,
+        Key: {
+            pk: account.account_id,
+            sk: account.account_id
+        }
+    }))
+    
+    account = account_details.Item
+    
+
+    let result = {
+        partner: user,
+        name: account.name,
+        balance: account.balance,
+        last_modified: account.last_modified
+    }
+
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(result),
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+        }
+
+    };
+
+    return response;
+}
